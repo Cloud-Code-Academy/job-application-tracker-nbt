f9eca2a7bac34fba0d6785b73747b7df39f28e1e
brandonedoria <brandonedoria@gmail.com>
2025-04-17 16:52:24 -0700

    Calculates correct values on recalculation, numbers formatted correctly into currency format

--- force-app/main/default/lwc/paycheckBreakdownCalculator/paycheckBreakdownCalculator.js
+++ force-app/main/default/lwc/paycheckBreakdownCalculator/paycheckBreakdownCalculator.js
@@ -1,7 +1,7 @@
 import { LightningElement} from 'lwc';
 
 // Const object to store federal tax rates for 2025
-const federalTaxRates = [
+const federalTaxRates = Object.freeze([
     {lowerRange: 0, upperRange: 11925, rate: 0.10},
     {lowerRange: 11925, upperRange: 48475, rate: 0.12},
     {lowerRange: 48475, upperRange: 103350, rate: 0.22},
@@ -9,22 +9,22 @@ const federalTaxRates = [
     {lowerRange: 197300, upperRange: 250525, rate: 0.32},
     {lowerRange: 250525, upperRange: 626350, rate: 0.35},
     {lowerRange: 626350, upperRange: Infinity, rate: 0.37},
-];
+]);
 const medicareTaxRate = 0.0145;
 const socialSecurityTaxRate = 0.0620;
 const standardDeduction = 15000;
 
 export default class PaycheckBreakdownCalculator extends LightningElement {
     // Initialize salary variables connected to HTML
-    grossSalary = 0;  
-    annualSalary = 0;
-    sixMonthsSalary = 0;
-    monthlySalary = 0;
-    biWeeklySalary = 0;
+    grossSalary = null;  
+    annualSalary = null;
+    sixMonthsSalary = null;
+    monthlySalary = null;
+    biWeeklySalary = null;
     // Initialize tax variables connected to HTML
-    federalIncomeTax = 0;
-    medicareTax = 0;
-    socialSecurityTax = 0;
+    federalIncomeTax = null;
+    medicareTax = null;
+    socialSecurityTax = null;
 
     // Update grossSalary to input value when 'Calculate' button is clicked
     handleChange(event) {
@@ -32,13 +32,15 @@ export default class PaycheckBreakdownCalculator extends LightningElement {
     }
 
     calculatePaycheckBreakdown() {
+        // Reset federal tax if recalculate
+        this.federalIncomeTax = 0;
         // Initialize new variables
         let federalTaxAmount = 0;
         let adjustedGrossSalary = this.grossSalary - standardDeduction;
         // Loop through each federal tax bracket to tax for each range
-        for (const bracket of federalTaxRates) {
-            if (adjustedGrossSalary > bracket.lowerRange) {
-                federalTaxAmount = (Math.min(adjustedGrossSalary, bracket.upperRange) - bracket.lowerRange) * bracket.rate;
+        for (let taxBracket of federalTaxRates) {
+            if (adjustedGrossSalary > taxBracket.lowerRange) {
+                federalTaxAmount = (Math.min(adjustedGrossSalary, taxBracket.upperRange) - taxBracket.lowerRange) * taxBracket.rate;
                 this.federalIncomeTax += federalTaxAmount;
             }
         }
